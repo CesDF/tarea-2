3f95e152d3054a2d433e9a02252878eaeda5da0f
CesarDiaz <enzo.cdf@hotmail.com>
2018-01-11 12:19:52 +0000

    Commit inicial

---
+++ src/tarea.java
@@ -0,0 +1,12 @@
+
+public class tarea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bienvenido al sistema\nHoy es lunes\nSon las 08:00\nTienes 8 correos sin leer");
+
+
+	}
+
+}
